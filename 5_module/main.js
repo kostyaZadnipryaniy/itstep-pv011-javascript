c3fb2f47530ce9e1c00b84347ee19d6ad5091261
teacher <teacher@khestep.edu>
2021-10-27 20:44:37 +0300

    add 5_module-forms

---
+++ 5_module/main.js
@@ -0,0 +1,59 @@
+const loginForm = document.forms[0]
+
+// loginForm.addEventListener('submit', e => {
+//     e.preventDefault()
+
+//     const emailText = e.target.elements[0].value
+//     const passwordText = e.target.elements[1].value
+//     const rememberMe = e.target.elements[2].checked
+
+//     alert(JSON.stringify({ emailText, passwordText, rememberMe  }))
+// })
+
+// loginForm.addEventListener('submit', e => {
+//     e.preventDefault()
+
+//     const emailText = e.target.elements[0].value
+//     const passwordText = e.target.elements[1].value
+//     const confirmPassword = e.target.elements[2].value
+//     const rememberMe = e.target.elements[3].checked
+
+//     if (passwordText === confirmPassword) 
+//         console.log(JSON.stringify({ emailText, passwordText, rememberMe  }))
+// })
+
+// loginForm.addEventListener('submit', e => {
+//     e.preventDefault()
+
+//     const files = e.target.elements[0].files
+
+//     // FormData
+//     console.log({ files })    
+// })
+
+// loginForm.addEventListener('submit', e => {
+//     e.preventDefault()
+
+//     const country = e.target.elements[0].value
+
+//     // FormData
+//     console.log({ country  })    
+// })
+
+
+// loginForm.addEventListener('submit', e => {
+//     e.preventDefault()
+
+//     const country = e.target.elements['country'].value
+
+//     // FormData
+//     console.log({ country  })    
+// })
+
+
+// loginForm.addEventListener('submit', e => {
+//     e.preventDefault()
+
+//     // FormData
+//     console.log(e.target.elements)    
+// })
