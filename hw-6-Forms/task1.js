820b6430a1e2bfff88cc8abd1207ac0bbc430072
yaroslavsf <yaroslav1598753@gmail.com>
2021-10-29 17:20:48 +0300

    undone task 1

---
+++ hw-6-Forms/task1.js
@@ -0,0 +1,10 @@
+let buttonAddMessage = document.querySelector('buttonAddMessageToList')
+
+buttonAddMessage.addEventListener('click', () => {
+    let name = document.querySelector()
+})
+console.log(buttonAddMessage)
+
+
+// let ul = document.createElement('ul');
+// document.body.appendChild(ul);
