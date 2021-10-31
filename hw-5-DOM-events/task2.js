2b5a4ff8173565552f01b030d8c4c5f6387fe5c4
yaroslavsf <yaroslav1598753@gmail.com>
2021-10-31 18:35:34 +0200

    only task2 done

--- hw-5-DOM-events/task2.js
+++ hw-5-DOM-events/task2.js
@@ -2,10 +2,10 @@ let element = document.querySelector(".hideable")
 
 //css left(making text bold with hoover)
 element.addEventListener("click", (e) => {
-    console.log(e.target.childNodes[1])
+    
     if (e.target.childNodes[1] != undefined)
     {
-        console.log(e.target.childNodes[1])
+    
         if (e.target.childNodes[1].style.display == "none") {
             e.target.childNodes[1].style.display = "block"
         } else {
